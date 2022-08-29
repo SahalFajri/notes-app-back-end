68ab1ac4994f4d78f13e215531c35e33615223c3
SahalFajri <sahalfajri879@gmail.com>
2022-08-29 22:25:19 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+// http://notesapp-v1.dicodingacademy.com/
+// npm install nanoid@3.x.x harus versi 3.x.x
+
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+}
+
+init();
